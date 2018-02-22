456bbe99cdd57fa87e8ab3e0097262eeccc74a59
ACA15JCH <jcdhan1@sheffield.ac.uk>
2018-02-22 17:52:36 +0000

    A catch-all pattern for the do/1 function

--- lab2/singleproc.erl
+++ lab2/singleproc.erl
@@ -1,12 +1,14 @@
 -module(singleproc).
 -export([do/1]).
 
-do({output , E}) ->
-	io:format("~~~p~n",[E]);
-do({input,E}) ->
-	io:format("~p~n",[E]);
-do([]) ->
-	ok;
-do([E | MoreEs]) ->
-	do(E),
-	do(MoreEs).
+%% do({output , E}) ->
+%% 	io:format("~~~p~n",[E]);
+%% do({input,E}) ->
+%% 	io:format("~p~n",[E]);
+%% do([]) ->
+%% 	ok;
+%% do([E | MoreEs]) ->
+%% 	do(E),
+%% 	do(MoreEs)."""
+do(_) ->
+	io:format("Unknown argument to do/1~n").
